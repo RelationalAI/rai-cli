0d979631972ea7cdb557005f3e0afce99e949651
andy-dean-rai <109111192+andy-dean-rai@users.noreply.github.com>
2022-11-16 20:47:53 -0500

    Update main.go

--- rai/main.go
+++ rai/main.go
@@ -150,7 +150,7 @@ func addCommands(root *cobra.Command) {
 		Short: "Create an OAuth client",
 		Args:  cobra.ExactArgs(1),
 		Run:   createOAuthClient}
-	cmd.Flags().String("perms", "", "permissionss")
+	cmd.Flags().String("perms", "", "permissions")
 	root.AddCommand(cmd)
 
 	cmd = &cobra.Command{
