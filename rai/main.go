c79e96ea09680c41de9189c48e04d0582b101697
Tania <124593929+stania-rai@users.noreply.github.com>
2023-05-17 10:47:53 -0400

    Fix typos

--- rai/main.go
+++ rai/main.go
@@ -297,9 +297,9 @@ func addCommands(root *cobra.Command) {
 		Args:  cobra.ExactArgs(1),
 		Run:   createSnowflakeIntegration}
 	cmd.Flags().String("account", "", "Snowflake account (default: SNOWSQL_ACCOUNT env var)")
-	cmd.Flags().String("admin-username", "", "Snowflake admin username (default: SNOWSQL_USER env var")
-	cmd.Flags().String("admin-password", "", "Snowflake admin password (default: SNOWSQL_PWD env var")
-	cmd.Flags().String("proxy-username", "", "Snowflake proxy username (default: SNOWSQL_USER env var")
+	cmd.Flags().String("admin-username", "", "Snowflake admin username (default: SNOWSQL_USER env var)")
+	cmd.Flags().String("admin-password", "", "Snowflake admin password (default: SNOWSQL_PWD env var)")
+	cmd.Flags().String("proxy-username", "", "Snowflake proxy username (default: SNOWSQL_USER env var)")
 	cmd.Flags().String("proxy-password", "", "Snowflake proxy password (default: SNOWSQL_PWD env var)")
 	root.AddCommand(cmd)
 
@@ -320,7 +320,7 @@ func addCommands(root *cobra.Command) {
 		Args:  cobra.ExactArgs(1),
 		Run:   deleteSnowflakeIntegration}
 	cmd.Flags().String("username", "", "Snowflake username (default: SNOWSQL_USER env var)")
-	cmd.Flags().String("password", "", "Snowflake password (default: SNOWSQL_PWD env var")
+	cmd.Flags().String("password", "", "Snowflake password (default: SNOWSQL_PWD env var)")
 	root.AddCommand(cmd)
 
 	cmd = &cobra.Command{
